ce23db67a6f858578e5c1e275c6eedcac612ae31
NeuronixLAB <p2pprogramm@mail.ru>
2026-09-13 14:55:13 +0700

    stockdata.py

--- stockdata.py
+++ stockdata.py
@@ -1,7 +1,10 @@
 from config import TOKEN
-from tinkoff.invest import Client, CandleInterval, Quotation
+from t_tech.invest import Client, CandleInterval, Quotation
 import pandas as pd
 from datetime import datetime, timedelta
+import os
+
+os.environ['SSL_TBANK_VERIFY'] = 'True'
 
 def quotation_to_float(num):
     return float(num.units) + float(num.nano) / 1e9
@@ -36,7 +39,26 @@ def df(figi: str):
     df = pd.DataFrame(data)
     df.set_index('date', inplace=True)
     df.index = pd.to_datetime(df.index)
-    df.index = df.index.tz_convert("Asia/Novosibirsk").tz_localize(None)
     return df['close']
 
-df = df(figi)
+def get_dates(figi: str):
+    data = []
+    try:
+        with Client(TOKEN) as client:
+            candles = client.get_all_candles(
+                figi=figi,
+                from_=datetime.now() - timedelta(days=10000),
+                to=datetime.now(),
+                interval=CandleInterval.CANDLE_INTERVAL_DAY
+            )
+            for candle in candles:
+                data.append({
+                    'date': candle.time
+                })
+            data = pd.DataFrame(data=data, columns=['date'])
+            return data
+    except Exception as e:
+        pass
+
+df = df(figi=figi)
+dates = get_dates(figi=figi)
